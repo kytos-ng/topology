e12c9f96edb15b48af150a3189c5247f7ac58452
Vinicius Arcanjo <viniarck@gmail.com>
2021-12-21 13:01:42 -0300

    Bumped version 3.7.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if 'bdist_wheel' in sys.argv:
 BASE_ENV = Path(os.environ.get('VIRTUAL_ENV', '/'))
 
 NAPP_NAME = 'topology'
-NAPP_VERSION = '3.7.2'
+NAPP_VERSION = '3.7.3'
 
 # Kytos var folder
 VAR_PATH = BASE_ENV / 'var' / 'lib' / 'kytos'
