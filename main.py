6f970a5ed3784e77a0e9dc7b884366788d62e0b4
Vinicius Arcanjo <vindasil@fiu.edu>
2023-06-19 11:45:05 -0300

    fix: included notify_topology_update before early return

--- main.py
+++ main.py
@@ -831,6 +831,7 @@ class Main(KytosNApp):  # pylint: disable=too-many-public-methods
                 else link.endpoint_a
             )
             if other_interface.is_active() is False:
+                self.notify_topology_update()
                 return
             metadata = {
                 'last_status_change': time.time(),
