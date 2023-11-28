e975abb37ae1872d6fec5146dc946ef98164297a
Aldo Ortega <aortegay@fiu.edu>
2023-11-28 11:49:49 -0500

    Added script

--- scripts/README.md
+++ scripts/README.md
@@ -163,7 +163,7 @@ Modified 0 switches objects
     tag_ranges = {"vlan": [[1, 4095]]}
 ```
 
-This scripts takes into account UNIs TAG values as well.
+This scripts takes into account UNIs TAG (only integers) values as well.
 
 #### Pre-requisites
 
@@ -228,6 +228,73 @@ An `ERROR` can be shown if a duplicated `TAG` is detected in different `EVC`s. A
 
 ```
 Error: Detected duplicated 200 TAG in EVCs 861a11d8fce148 and d74e18464d524b in interface 00:00:00:00:00:00:00:01:1
+```
+
+</details>
+
+<details><summary><h3>Add <code>special_available_tags</code> field to each Interface document in <code>interface_details</code> collection </h3></summary>
+
+[`special_vlan_allocation.py`](./special_vlan_allocation.py) is to add the new field ``special_available_tags`` to each interface document. This new field will keep track of special vlan usage:
+
+```
+special_available_tags = {"vlan": ["untagged", "any"]}
+```
+
+This scripts takes into account UNIs TAG values (only string) as well.
+
+#### Pre-requisites
+
+- There's no additional Python libraries dependencies required, other than installing the existing `topology`'s, or if you're running in development locally then installing `requirements/dev.in`
+- Make sure you don't have `kytosd` running with otherwise topology will start writing to MongoDB, and the application could overwrite the data you're trying to insert with this script.
+- Make sure MongoDB replica set is up and running.
+- Export the following MongnoDB variables accordingly in case your running outside of a container
+
+```
+export MONGO_USERNAME=
+export MONGO_PASSWORD=
+export MONGO_DBNAME=napps
+export MONGO_HOST_SEEDS="mongo1:27017,mongo2:27018,mongo3:27099"
+```
+
+#### How to use
+
+- The following `CMD` commands are available:
+
+```
+aggregate_outdated_interfaces
+update_database
+```
+
+`aggregate_outdated_interfaces` option is to see how many documents are going to be modified and how many are going to be added.
+
+```
+CMD=aggregate_outdated_interfaces python3 scripts/special_vlan_allocation.py
+```
+
+For the interfaces that are going to be modified, they are going to be listed:
+
+```
+There are 1 outdated interface documents which do not have 'special_available_tags' field:
+00:00:00:00:00:00:00:02:3
+```
+
+`update_database` updates and adds the required documents for compatability
+
+```
+CMD=update_database python3 scripts/special_vlan_allocation.py
+```
+
+The final messages will show how many interfaces have been modified:
+
+```
+1 interface was/were updated:
+00:00:00:00:00:00:00:02:3
+```
+
+An `ERROR` can be shown if a duplicated `TAG` is detected in different `EVC`s. After this the pocess will exit without making any modification or adittion.
+
+```
+Error: Detected duplicated vlan 'any' TAG in EVCs d68eb033688a48 and 861a11d8fce148 in interface 00:00:00:00:00:00:00:01:1
 ```
 
 </details>
---
+++ scripts/special_vlan_allocation.py
@@ -0,0 +1,106 @@
+import datetime
+import sys
+import os
+from collections import defaultdict
+from kytos.core.db import Mongo
+
+
+def aggregate_outdated_interfaces(mongo: Mongo):
+    """Aggregate outdated interfaces details"""
+    db = mongo.client[mongo.db_name]
+    outdated_intfs = set()
+    result = db.interface_details.aggregate(
+        [
+            {"$project": {
+                "_id": 0,
+                "id": 1,
+                "special_available_tags": 1,
+            }}
+        ]
+    )
+    for document in result:
+        if not "special_available_tags" in document:
+            outdated_intfs.add(document["id"])
+    
+    if outdated_intfs:
+        print(f"There are {len(outdated_intfs)} outdated interface documents"
+              " which do not have 'special_available_tags' field:")
+        for intf_id in outdated_intfs:
+            print(intf_id)
+    else:
+        print("All interfaces are updated.")
+
+def update_database(mongo: Mongo):
+    db = mongo.client[mongo.db_name]
+    intfs_documents = db.interface_details.find()
+    evc_documents = db.evcs.find({"archived": False})
+
+    tag_by_intf = defaultdict(set)
+    evc_intf = defaultdict(str)
+
+    for evc in evc_documents:
+        tag_a = evc["uni_a"].get("tag")
+        if tag_a and isinstance(tag_a["value"], str):
+            intf_id = evc["uni_a"]["interface_id"]
+            if tag_a["value"] in tag_by_intf[intf_id]:
+                print(f"Error: Detected duplicated vlan '{tag_a['value']}' TAG"
+                      f" in EVCs {evc['id']} and {evc_intf[intf_id+tag_a['value']]}"
+                      f" in interface {intf_id}")
+                sys.exit(1)
+            tag_by_intf[intf_id].add(tag_a["value"])
+            evc_intf[intf_id+tag_a["value"]] = evc["id"]
+        tag_z = evc["uni_z"].get("tag")
+
+        if tag_z and isinstance(tag_z["value"], str):
+            intf_id = evc["uni_z"]["interface_id"]
+            if tag_z["value"] in tag_by_intf[intf_id]:
+                print(f"Error: Detected duplicated vlan '{tag_z['value']}' TAG"
+                      f" in EVCs {evc['id']} and {evc_intf[intf_id+tag_z['value']]}"
+                      f" in interface {intf_id}")
+                sys.exit(1)
+            tag_by_intf[intf_id].add(tag_z["value"])
+            evc_intf[intf_id+tag_z["value"]] = evc["id"]
+
+    default_special_vlans = {"untagged", "any"}
+    intf_count = 0
+    message_intfs = ""
+    for intf in intfs_documents:
+        _id = intf["id"]
+        current_field = intf.get("special_available_tags", None)
+        if current_field:
+            current_field = set(current_field["vlan"])
+        expected_field = default_special_vlans - tag_by_intf.get(_id, set())
+        if current_field == expected_field:
+            continue
+        db.interface_details.update_one(
+            {"id": _id},
+            {
+                "$set":
+                {
+                    "special_available_tags": {"vlan": list(expected_field)}
+                }
+            }
+        )
+        message_intfs += f"{_id}\n"
+        intf_count += 1
+    if intf_count:
+        print(f"{intf_count} interface was/were updated:")
+        print(message_intfs)
+    else:
+        print("All interfaces are updated already.")
+
+
+if __name__ == "__main__":
+    mongo = Mongo()
+    cmds = {
+        "aggregate_outdated_interfaces": aggregate_outdated_interfaces,
+        "update_database": update_database,
+    }
+    try:
+        cmd = os.environ["CMD"]
+        cmds[cmd](mongo)
+    except KeyError:
+        print(
+            f"Please set the 'CMD' env var. \nIt has to be one of these: {list(cmds.keys())}"
+        )
+        sys.exit(1)
