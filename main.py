72b129f8a3ee0bbf13b7284cf6cbd18972aed7e3
Vinicius Arcanjo <vindasil@fiu.edu>
2024-09-10 12:48:56 -0300

    feat: included link changed status log info for correlation

--- main.py
+++ main.py
@@ -1228,6 +1228,8 @@ class Main(KytosNApp):  # pylint: disable=too-many-public-methods
                 (not link.status_reason and link.status == EntityStatus.UP)
                 and link_id not in self.link_up
             ):
+                log.info(f"{link} changed status {link.status}, "
+                         f"reason: {reason}")
                 self.link_up.add(link_id)
                 event = KytosEvent(
                     name='kytos/topology.link_up',
@@ -1240,6 +1242,8 @@ class Main(KytosNApp):  # pylint: disable=too-many-public-methods
                 (link.status_reason or link.status != EntityStatus.UP)
                 and link_id in self.link_up
             ):
+                log.info(f"{link} changed status {link.status}, "
+                         f"reason: {reason}")
                 self.link_up.remove(link_id)
                 event = KytosEvent(
                     name='kytos/topology.link_down',
