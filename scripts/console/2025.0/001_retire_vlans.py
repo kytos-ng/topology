2ef684e04c692847f9d459c5c6378086213e3744
David Ramirez <davramir@fiu.edu>
2025-03-28 16:54:03 -0400

    Improved retire_vlans script stability with thread pools

--- scripts/console/2025.0/001_retire_vlans.py
+++ scripts/console/2025.0/001_retire_vlans.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import concurrent.futures
 
 import kytos.core.tag_ranges as range_helpers
 
@@ -14,7 +15,10 @@ def disable_evc(evc_id):
     data = {
         "enabled": False
     }
-    res = httpx.request("PATCH", url, json=data, timeout=10)
+    try:
+        res = httpx.request("PATCH", url, json=data, timeout=30)
+    except httpx.TimeoutException:
+        print(f"Timeout while enabling EVC {evc_id}")
     if res.is_server_error or res.status_code in {424, 404, 400}:
         print(f"Error disabling EVC {evc_id}: {res.text}")
 
@@ -25,9 +29,12 @@ def enable_evc(evc_id):
     data = {
         "enabled": True
     }
-    res = httpx.request("PATCH", url, json=data, timeout=30)
+    try:
+        res = httpx.request("PATCH", url, json=data, timeout=30)
+    except httpx.TimeoutException:
+        print(f"Timeout while enabling EVC {evc_id}")
     if res.is_server_error or res.status_code in {424, 404, 400}:
-        print(f"Error disabling EVC {evc_id}: {res.text}")
+        print(f"Error enabling EVC {evc_id}: {res.text}")
 
 mef_eline = controller.napps[("kytos", "mef_eline")]
 of_lldp = controller.napps[('kytos', 'of_lldp')]
@@ -97,8 +104,9 @@ for intf_id in in_use_tags:
 # Disable EVCs
 if not DRY_RUN and evc_disable_set:
     print("Disabling EVCs...")
-    for evc_id in evc_disable_set:
-            disable_evc(evc_id)
+    executor = concurrent.futures.ThreadPoolExecutor(10, "script:disable_evc")
+    executor.map(disable_evc, evc_disable_set)
+    executor.shutdown()
 
 # Retire the given vlan ranges
 
@@ -146,7 +154,8 @@ for dpid in list(controller.switches.keys()):
 
 if not DRY_RUN and evc_disable_set:
     print("Re-enabling EVCs...")
-    for evc_id in evc_disable_set:
-            enable_evc(evc_id)
+    executor = concurrent.futures.ThreadPoolExecutor(10, "script:enable_evc")
+    executor.map(enable_evc, evc_disable_set)
+    executor.shutdown()
 
 print("Finished!")
